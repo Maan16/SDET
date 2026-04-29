d7f17c64be42ecc3f29c02e6fe570744ff23060a
Maan16 <106450047+Maan16@users.noreply.github.com>
2026-04-29 12:30:05 +0100

    Javascript Object Syntax Practice

--- javascript-practice/src/index.js
+++ javascript-practice/src/index.js
@@ -1,8 +1,12 @@
-
 let text = "";
 
 for (let i = 0; i < 5; i++) {
   text += "The number is " + i + "\n";
 }
 
-console.log("text:::::",text);
+console.log("text:::::", text);
+
+let a = 10;
+let b = 20;
+let c = (a+=b);
+console.log(c+a);
---
+++ javascript-practice/src/myObject.js
@@ -0,0 +1,22 @@
+//Practice of creating an objetc in JS.
+//An object usually has properties and methods.
+//  Properties are like variables that belong to
+// the object, and methods are functions that belong to the object.
+
+const firstObj = {
+  firsrtName: "John",
+  lastName: "Swain",
+  age: 30,
+  isPass: "true",
+};
+console.log(firstObj);
+
+//We usually use const to declare objec but just for practice i have done with let as well
+
+let secondObj = {
+  firstName: "Jane",
+  lastName: "Doe",
+  age: 25,
+  isPass: "false",
+};
+console.log(secondObj);
